9113719a75442fd7441b78b54e847ec438b48fe9
xinjiprincess <56882651+xinjiprincess@users.noreply.github.com>
2021-06-17 12:55:12 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<windows.h>
+#include<time.h>
+#include<stlidb.h>
+#include<string>
+int main(void)
+{
+  cout<<"正在载入周方编辑器，全语言支持编辑器"<<endl;
+   int i=0;
+  for(i=0;i<10;i++)
+  {
+    cout<<"<<"<<;
+    sleep(1000);
+  }
+  cout<<"载入成功"<<endl;
+  string sbcodes;
+  cout<<"现在输入代码吧"<<;
+  cin>>sbcodes>>;
+  cout<<"编译中"<<;
+  sleep(10000);
+  cout<<"编译成功"<<endl;
+}
